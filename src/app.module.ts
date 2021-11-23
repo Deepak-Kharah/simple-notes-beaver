81cc4ae8ab4696c69ccfcdf0d616e803e71cde94
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-11-24 00:04:29 +0530

    chore(config): added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+      load: [configuration],
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  //@ts-ignore
+  console.log(configService.get('DATABASE_STRING'));
+
+  //@ts-ignore
+  await app.listen(configService.get('port'));
 }
 bootstrap();
