48eff0afa44f7f9896da1b56d5bf471754b3d21d
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-11-24 18:36:14 +0530

    added(database): mongodb support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import configuration from 'config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,8 +12,10 @@ import { AppService } from './app.service';
       load: [configuration],
       expandVariables: true,
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest', {}),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+console.log(process.env.DATABASE_STRING);
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import configuration from 'config/configuration';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-
-  //@ts-ignore
-  console.log(configService.get('DATABASE_STRING'));
 
   //@ts-ignore
   await app.listen(configService.get('port'));
